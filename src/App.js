49c9a973f26d92faf3cea6178179c3ecc13f5619
Gabo <gabrielathie@hotmail.com>
2025-10-28 21:14:41 -0600

    Fix for auth page
    Remove nav bar on auth page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import TopNavBar from './components/TopNavBar';
 import SideMenu from './components/SideMenu';
@@ -8,8 +8,9 @@ import RecipesPage from './components/pages/RecipesPage';
 import AuthPage from './components/pages/AuthPage';
 import ProtectedRoute from './components/ProtectedRoute';
 
-function App() {
+function AppContent() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -19,25 +20,40 @@ function App() {
     setIsMenuOpen(false);
   };
 
+  // Check if current route is auth page
+  const isAuthPage = location.pathname === '/auth';
+
+  return (
+    <div className="App">
+      {/* Only show navigation on non-auth pages */}
+      {!isAuthPage && (
+        <>
+          <TopNavBar onMenuToggle={toggleMenu} isMenuOpen={isMenuOpen} />
+          <SideMenu isOpen={isMenuOpen} onClose={closeMenu} />
+        </>
+      )}
+      
+      <Routes>
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        } />
+        <Route path="/recetas" element={
+          <ProtectedRoute>
+            <RecipesPage />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </div>
+  );
+}
+
+function App() {
   return (
     <Router>
-      <div className="App">
-        <TopNavBar onMenuToggle={toggleMenu} isMenuOpen={isMenuOpen} />
-        <SideMenu isOpen={isMenuOpen} onClose={closeMenu} />
-        <Routes>
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/" element={
-            <ProtectedRoute>
-              <HomePage />
-            </ProtectedRoute>
-          } />
-          <Route path="/recetas" element={
-            <ProtectedRoute>
-              <RecipesPage />
-            </ProtectedRoute>
-          } />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
